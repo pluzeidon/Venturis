3dc4482adf4a78ae8f0254a8d19c8d44a8923b6c
pluzeidon <45407966+pluzeidon@users.noreply.github.com>
2019-08-20 21:37:59 -0500

    6ta

--- Venturis/Venturis/App.xaml.cs
+++ Venturis/Venturis/App.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using Venturis.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -11,7 +11,7 @@ namespace Venturis
         {
             InitializeComponent();
 
-             MainPage = new NavigationPage(new MainPage());
+            MainPage = new NavigationPage(new VenturisNavigator());
 
             //MainPage = new NavigationPage(new Views.VenturisOriginal("https://www.venturisapp.net/ords/pdb1/f?p=111"));
         }
---
+++ Venturis/Venturis/Views/VenturisNavigator.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ZXing.Net.Mobile.Forms;
+
+namespace Venturis.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VenturisNavigator : ContentPage
+    {
+        string Url;
+
+        public VenturisNavigator()
+        {
+            InitializeComponent();
+            cwv.Source = Url = GetUrl();
+        }
+        private void Cwv_InitScan(object sender, EventArgs e)
+        {
+            Scanner();
+        }
+
+        private async void Scanner()
+        {
+            string _result = string.Empty;
+            var scannerPage = new ZXingScannerPage();
+            scannerPage.Title = "Presente el codigo de barras";
+            scannerPage.OnScanResult += (result) =>
+            {
+                scannerPage.IsScanning = false;
+                Task.Run(() => Device.BeginInvokeOnMainThread(() =>
+                {
+                    Navigation.PopAsync();
+                    //_result = result.Text;
+                    cwv.SetTextScaned(result.Text);
+
+                    //string strDireccion = cwv.Source.GetValue(UrlWebViewSource.UrlProperty).ToString();
+
+                    //string strResultado = Base64Encode(result.Text);
+
+                    //if (strResultado != "")
+                    //    strDireccion = strDireccion + strResultado;
+
+                    //cwv.Source = strDireccion;
+                }));
+            };
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                Navigation.PushAsync(scannerPage);
+            });
+        }
+
+        static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+        void OnBack(object sender, EventArgs args)
+        {
+            cwv.GoBack();
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            if (cwv.CanGoBack)
+            {
+                cwv.GoBack();
+                return true;
+            }
+            else return base.OnBackButtonPressed();
+        }
+
+        void OnNavigating(object sender, WebNavigatingEventArgs args)
+        {
+            // Comprobando si estamos en la URL de la página de inicio
+            // browser.CanGoBack no parece funcionar (no se actualiza a tiempo)
+            NavigationPage.SetHasNavigationBar(this, args.Url != Url);
+        }
+
+        string GetUrl()
+        {
+            return "https://www.venturisapp.net/ords/pdb1/f?p=111";
+        }
+    }
+}
