3e2307e502a4ad0fda1766b1bb0fd2c10dbe8873
pluzeidon <45407966+pluzeidon@users.noreply.github.com>
2019-08-29 16:32:18 -0500

    lo mismo

--- Venturis/Venturis.iOS/AppDelegate.cs
+++ Venturis/Venturis.iOS/AppDelegate.cs
@@ -23,7 +23,7 @@ namespace Venturis.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
-            ZXing.Net.Mobile.Forms.iOS.Platform.Init();
+            global::ZXing.Net.Mobile.Forms.iOS.Platform.Init();
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
