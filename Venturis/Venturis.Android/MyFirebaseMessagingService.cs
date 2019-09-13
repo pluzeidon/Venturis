378cf3a1b24ed8d0b5138a45c2a965fbb30f908c
pluzeidon <45407966+pluzeidon@users.noreply.github.com>
2019-09-13 10:32:20 -0500

    Primera version con notificaciones

--- Venturis/Venturis.Android/MyFirebaseMessagingService.cs
+++ Venturis/Venturis.Android/MyFirebaseMessagingService.cs
@@ -6,6 +6,8 @@ using Android.Support.V4.App;
 using WindowsAzure.Messaging;
 using Android.App;
 using Android.Content;
+using Android.OS;
+using Android.Graphics;
 
 namespace Venturis.Droid
 {
@@ -19,18 +21,10 @@ namespace Venturis.Droid
 
         public override void OnMessageReceived(RemoteMessage message)
         {
-            Log.Debug(TAG, "From: " + message.From);
-            if (message.GetNotification() != null)
-            {
-                //These is how most messages will be received
-                Log.Debug(TAG, "Notification Message Body: " + message.GetNotification().Body);
-                SendNotification(message.GetNotification().Body);
-            }
+            if (!message.Data.GetEnumerator().MoveNext())
+                SendNotification(message.GetNotification().Title, message.GetNotification().Body);
             else
-            {
                 SendNotification(message.Data);
-
-            }
         }
 
         private void SendNotification(IDictionary<string, string> data)
@@ -38,33 +32,41 @@ namespace Venturis.Droid
             string title, body;
             data.TryGetValue("title", out title);
             data.TryGetValue("body", out body);
-            SendNotification( body);
+            SendNotification(title, body);
         }
 
-
-        void SendNotification(string messageBody)
+        private void SendNotification(string title, string body)
         {
-            var intent = new Intent(this, typeof(MainActivity));
-            intent.AddFlags(ActivityFlags.ClearTop);
-            var pendingIntent = PendingIntent.GetActivity(this, 0, intent, PendingIntentFlags.OneShot);
+            NotificationManager notificationManager = (NotificationManager)GetSystemService(Context.NotificationService);
 
-            var notificationBuilder = new NotificationCompat.Builder(this, MainActivity.CHANNEL_ID);
+            if (Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.O)
+            {
+                NotificationChannel notificationChannel = new NotificationChannel(MainActivity.CHANNEL_ID, "Notification Chanel", Android.App.NotificationImportance.Default);
+                notificationChannel.Description = "QDX Chanel";
+                notificationChannel.EnableLights(true);
+                notificationChannel.LightColor = Color.Blue;
+                notificationChannel.SetVibrationPattern(new long[] { 0, 1000, 500, 1000 });
+                notificationManager.CreateNotificationChannel(notificationChannel);
+            }
 
-            notificationBuilder.SetContentTitle("Venturis Expenses")
-                        .SetSmallIcon(Resource.Drawable.ic_launcher)
-                        .SetContentText(messageBody)
-                        .SetAutoCancel(true)
-                        .SetShowWhen(false)
-                        .SetContentIntent(pendingIntent);
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, MainActivity.CHANNEL_ID);
 
-            var notificationManager = NotificationManager.FromContext(this);
+            notificationBuilder.SetAutoCancel(true)
+                .SetDefaults(-1)
+                .SetWhen(DateTimeOffset.UtcNow.ToUnixTimeMilliseconds())
+                .SetContentTitle(title)
+                .SetContentText(body)
+                .SetSmallIcon(Resource.Drawable.Icon36)
+                .SetContentInfo("info");
 
-            notificationManager.Notify(0, notificationBuilder.Build());
+            notificationManager.Notify(new Random().Next(), notificationBuilder.Build());
+
+
         }
 
         public override void OnNewToken(string token)
         {
-            Log.Debug(TAG, "Venturis Expenses token: " + token);
+            Log.Debug(TAG, "FCM token: " + token);
             SendRegistrationToServer(token);
         }
 
@@ -79,7 +81,5 @@ namespace Venturis.Droid
 
             Log.Debug(TAG, $"Successful registration of ID {regID}");
         }
-
-
     }
 }
