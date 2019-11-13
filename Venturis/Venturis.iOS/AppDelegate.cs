5e853b9d1b8e322dfe847e024648c960cb98af44
pluzeidon <45407966+pluzeidon@users.noreply.github.com>
2019-11-13 14:20:22 -0600

    Cambois al 13 Nov 2019

--- Venturis/Venturis.iOS/AppDelegate.cs
+++ Venturis/Venturis.iOS/AppDelegate.cs
@@ -4,9 +4,12 @@ using System.Linq;
 
 using Foundation;
 using UIKit;
+using UserNotifications;
+using WindowsAzure.Messaging;
 
 namespace Venturis.iOS
 {
+    
     // The UIApplicationDelegate for the application. This class is responsible for launching the 
     // User Interface of the application, as well as listening (and optionally responding) to 
     // application events from iOS.
@@ -26,7 +29,78 @@ namespace Venturis.iOS
             global::ZXing.Net.Mobile.Forms.iOS.Platform.Init();
             LoadApplication(new App());
 
-            return base.FinishedLaunching(app, options);
+            //return base.FinishedLaunching(app, options);
+            base.FinishedLaunching(app, options);
+
+            RegisterForRemoteNotifications();
+
+            return true;
+        }
+
+        void RegisterForRemoteNotifications()
+        {   
+            // register for remote notifications based on system version
+            if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
+            {
+                UNUserNotificationCenter.Current.RequestAuthorization(UNAuthorizationOptions.Alert |
+                    UNAuthorizationOptions.Sound |
+                    UNAuthorizationOptions.Sound,
+                    (granted, error) =>
+                    {
+                        if (granted)
+                            InvokeOnMainThread(UIApplication.SharedApplication.RegisterForRemoteNotifications);
+                    });
+            }
+            else if (UIDevice.CurrentDevice.CheckSystemVersion(8, 0))
+            {
+                var pushSettings = UIUserNotificationSettings.GetSettingsForTypes(
+                UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound,
+                new NSSet());
+
+                UIApplication.SharedApplication.RegisterUserNotificationSettings(pushSettings);
+                UIApplication.SharedApplication.RegisterForRemoteNotifications();
+            }
+            else
+            {
+                UIRemoteNotificationType notificationTypes = UIRemoteNotificationType.Alert | UIRemoteNotificationType.Badge | UIRemoteNotificationType.Sound;
+                UIApplication.SharedApplication.RegisterForRemoteNotificationTypes(notificationTypes);
+            }
+        }
+
+        public override void RegisteredForRemoteNotifications(UIApplication application, NSData deviceToken)
+        {
+            SBNotificationHub Hub = new SBNotificationHub(ConstantsIOS.ConnectionString, ConstantsIOS.NotificationHubPath);
+
+            // update registration with Azure Notification Hub
+            Hub.UnregisterAllAsync(deviceToken, (error) =>
+            {
+                if (error != null)
+                {
+                    Debug.WriteLine($"Unable to call unregister {error}");
+                    return;
+                }
+
+                var tags = new NSSet(AppConstants.SubscriptionTags.ToArray());
+                Hub.RegisterNativeAsync(deviceToken, tags, (errorCallback) =>
+                {
+                    if (errorCallback != null)
+                    {
+                        Debug.WriteLine($"RegisterNativeAsync error: {errorCallback}");
+                    }
+                });
+
+                var templateExpiration = DateTime.Now.AddDays(120).ToString(System.Globalization.CultureInfo.CreateSpecificCulture("en-US"));
+                Hub.RegisterTemplateAsync(deviceToken, "defaultTemplate", AppConstants.APNTemplateBody, templateExpiration, tags, (errorCallback) =>
+                {
+                    if (errorCallback != null)
+                    {
+                        if (errorCallback != null)
+                        {
+                            Debug.WriteLine($"RegisterTemplateAsync error: {errorCallback}");
+                        }
+                    }
+                });
+            });
         }
     }
 }
---
+++ Venturis/Venturis.iOS/ConstantsIOS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+
+namespace Venturis.iOS
+{
+    public class ConstantsIOS
+    {
+        public const string ConnectionString = "Endpoint=sb://venturisexpenses.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=t2OjoXvGW1mO+OIKav1MKNzGsp5AgH1J1xNw/ElVG4U=";
+        public const string NotificationHubPath = "VenturisExpenses";
+    }
+}
